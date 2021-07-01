c3be40bfec5a8edc479e0aa5be70591eed08209c
Yisrael Gurkow <yisrelg3@gmail.com>
2021-07-01 14:34:32 -0400

    commit tree c0141ee0ecf653df129508a3286eee270007734a

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
   
   def show
     author = Author.find(params[:id])
@@ -7,15 +8,18 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.create!(author_params)
+    render json: author, status: :created      
   end
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def invalid_response(invalid)
+    render json: {"errors": invalid.record.errors}, status: :unprocessable_entity
   end
   
 end
